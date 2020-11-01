349bb2808c7c81b3043328ee578990860600959b
Tintinnabulate <tintinn@abulate>
2020-11-01 16:09:48 +0000

    Fix script and get it working

--- script.js
+++ script.js
@@ -1,28 +1,30 @@
 $(document)
     .ready(function() {
-        $.ajax({
-            url: 'https://raw.githubusercontent.com/tintinnabulate/intergroup-dashboard/master/output.json',
-            method: 'get',
-            dataType: 'json',
-            success: function(data) {
-                var exampleTable = $('#example')
-                    .DataTable({
-                      data: data,
-                      "columns": [
-                                   { "data": "code"},
-                                   { "data":  "day" },
-                                   { "data":  "hearing" },
-                                   { "data":  "lat" },
-                                   { "data":  "lng" },
-                                   { "data":  "postcode" },
-                                   { "data":  "time" },
-                                   { "data":  "duration" },
-                                   { "data":  "title" },
-                                   { "data":  "wheelchair" },
-                                   { "data":  "covid_open_status" },
-                                   { "data":  "detail" },
-                                   { "data":  "conference_url" }
-                                 ]
-					} );
-				} });
+		$('#example').dataTable( {
+        	"ajax": {
+            "url": 'https://raw.githubusercontent.com/tintinnabulate/intergroup-dashboard/master/output.json',
+            "type": "GET",
+            "cache": true,
+            "complete":
+				function(xhr, status) {
+					console.log(xhr.responseText);
+                    console.log(status);
+				}
+            },
+            "columns": [
+                         { "data": "code"},
+                         { "data": "day" },
+                         { "data": "hearing" },
+                         { "data": "lat" },
+                         { "data": "lng" },
+                         { "data": "postcode" },
+                         { "data": "time" },
+                         { "data": "duration" },
+                         { "data": "title" },
+                         { "data": "wheelchair" },
+                         { "data": "covid_open_status" },
+                         { "data": "detail" },
+                         { "data": "conference_url" }
+                       ],
 			} );
+});
